b093a7d7f770ded97b921009eb8327781db3639a
Egert Tappo <egert.tappo@tktk.ee>
2023-10-11 20:28:47 +0300

    Create new app

---
+++ Myfirstapp/Program.cs
@@ -0,0 +1,29 @@
+﻿// MyFirstApp
+//Console.WriteLine("Hello, õppija!");
+//Console.WriteLine("Tere, minu nimi on MVS." + "   Aga sinu?"); //Output 
+
+
+//Nime küsimine variant 1
+//string userName;
+//userName = Console.ReadLine();
+
+//Nime küsimine Variant 2
+//string userName = Console.ReadLine(); //Input
+
+
+
+//Nime kirjutamine konsoolis variant 1
+
+//Console.WriteLine("Tere, " + userName + "!");
+//string-sõne interpolation/ userName süstimine, Variant 2
+//Console.WriteLine($"Hello, {userName}!");
+
+string firstName, lastName;
+
+firstName = "Harry";
+lastName = "Potter";
+
+Console.WriteLine($"Hello, {firstName} {lastName} !");
+
+
+Console.WriteLine($"Hello, " + firstName +        lastName + "!");
